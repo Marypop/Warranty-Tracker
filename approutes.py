2414874f9f17ebf174557d1f5c7ecd8ab3dd2ee5
shreyas <shreyaspatil87@gmail.com>
2015-11-30 14:44:23 +0530

    Added routes and started with user screen template

--- approutes.py
+++ approutes.py
@@ -10,6 +10,14 @@ from flask import render_template, request
 warApp = WarrantyApp()
 app = warApp.app
 
+
+# This will be the home screen of the app
 @app.route('/')
 def indexScreen():
     return(render_template('index.html'))
+
+
+# This route will get executed while user is logged in the app
+@app.route('/home/<user_name>')
+def displayUserScreen(user_name):
+    return(render_template('user_screen.html', user_name=user_name))
