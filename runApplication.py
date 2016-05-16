7d2e9dc70a6b2875110730e41020225236920cf2
shreyas <shreyas@erpnext.com>
2016-05-16 22:53:59 +0530

    [Fix] Removed ProxyFix

--- runApplication.py
+++ runApplication.py
@@ -9,15 +9,11 @@ __author__ = 'shreyas'
 
 from flask import Flask
 from appRoutes import *
-from werkzeug.contrib.fixers import ProxyFix
 
 class WarrantyApp:
     # Initialize the class instance
     def __init__(self):
         self.app = Flask(__name__)
-
-        # ProxyFix
-        self.app.wsgi_app = ProxyFix(self.app.wsgi_app)
         self.wsgi_app = self.app.wsgi_app
 
 
